311c3ef637f7405eaa219cf0a30454c18e4504f6
skvoruha <skvoruha@mail.ru>
2021-03-09 20:08:36 +0300

    create: slider switch with the arrows

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-
+  slidesPerView: 1,
+      // spaceBetween: 30,
+      keyboard: {
+        enabled: true,
+      },
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
