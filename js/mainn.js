d2733909c70c09bd1231aee55bf42cb1899fceb9
skvoruha <skvoruha@mail.ru>
2021-03-10 20:27:39 +0300

    Create: Js-code slider and media -photo

--- js/mainn.js
+++ js/mainn.js
@@ -1,10 +1,15 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-
+   keyboard: {
+    // enabled: true,
+    ArrowLeft: true,
+    ArrowRight: true
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
 });
+
