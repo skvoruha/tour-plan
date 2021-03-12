18473b45b1f2c7ae5037838c9a7cc1100ceaba58
skvoruha <skvoruha@mail.ru>
2021-03-12 15:17:04 +0300

    create: saded reviews section and fix hotel-slider

--- js/mainn.js
+++ js/mainn.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
    keyboard: {
@@ -8,8 +8,16 @@ const swiper = new Swiper('.swiper-container', {
   },
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
+var reviewsSlider = new Swiper('.reviews-slider', {
+   loop: true,
+
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 });
 ymaps.ready(init);
